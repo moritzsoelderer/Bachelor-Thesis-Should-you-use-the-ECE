5718f33513c1e228ef6b3c16d86e7324cd8adb9d
moritzsoelderer <moritzsoelderer@gmail.com>
2024-05-05 22:02:22 +0200

    Extended experiment by list of test_examples

--- main.py
+++ main.py
@@ -56,31 +56,32 @@ for example in test_examples:
 ### experiments
 
 # do conditional probabilities provided by calc_prob coincide with relative frequencies of randomly drawn samples?
-n_examples = 10000000
+n_examples = 100000
 samples = []
 
-test_example = 0
-delta = .01  # delta inaccuracy for sample selection
-samples_near_test_example = []
+test_examples = [-10, -5, -2, -1, -0.5, -0.25, 0, 0.25, 0.5]
 
-for dist_list in dists:
-    samples_list = list(map(lambda distribution: sorted(distribution.rvs(size=n_examples)), dist_list))
-    samples.append(samples_list)
-    samples_list_near_test_example = \
-        list(map(lambda sample: len(
-            [example for example in sample if test_example + delta >= example >= test_example - delta]),
-                 samples_list))
-    samples_near_test_example.append(samples_list_near_test_example)
-    print(samples_list_near_test_example)
+for test_example in test_examples:
+    delta = .01  # delta inaccuracy for sample selection
+    samples_near_test_example = []
 
-test_example_cond_prob0 = calc_prob(test_example, 0)
-test_example_cond_prob1 = calc_prob(test_example, 1)
+    for dist_list in dists:
+        samples_list = list(map(lambda distribution: sorted(distribution.rvs(size=n_examples)), dist_list))
+        samples.append(samples_list)
+        samples_list_near_test_example = \
+            list(map(lambda sample: len(
+                [example for example in sample if test_example + delta >= example >= test_example - delta]),
+                     samples_list))
+        samples_near_test_example.append(samples_list_near_test_example)
 
-test_example_rel_freq0 = (
-        sum(samples_near_test_example[0]) / (sum(samples_near_test_example[0]) + sum(samples_near_test_example[1])))
-test_example_rel_freq1 = (
-        sum(samples_near_test_example[1]) / (sum(samples_near_test_example[0]) + sum(samples_near_test_example[1])))
+    test_example_cond_prob0 = calc_prob(test_example, 0)
+    test_example_cond_prob1 = calc_prob(test_example, 1)
 
-print("Test example:", test_example)
-print("Class 0:", "Cond. Prob.:", test_example_cond_prob0, " - ", "Rel. Freq.:", test_example_rel_freq0)
-print("Class 1:", "Cond. Prob.:", test_example_cond_prob1, " - ", "Rel. Freq.:", test_example_rel_freq1)
+    test_example_rel_freq0 = (
+            sum(samples_near_test_example[0]) / (sum(samples_near_test_example[0]) + sum(samples_near_test_example[1])))
+    test_example_rel_freq1 = (
+            sum(samples_near_test_example[1]) / (sum(samples_near_test_example[0]) + sum(samples_near_test_example[1])))
+
+    print("Test example:", test_example)
+    print("Class 0:", "Cond. Prob.:", test_example_cond_prob0, " - ", "Rel. Freq.:", test_example_rel_freq0)
+    print("Class 1:", "Cond. Prob.:", test_example_cond_prob1, " - ", "Rel. Freq.:", test_example_rel_freq1)
