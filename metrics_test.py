ad5edaf80e3b780e19e9c6eadab7bcf313d71647
moritzsoelderer <moritzsoelderer@gmail.com>
2024-05-30 16:27:07 +0200

    Added ksce methods and files from official github repository + tried derive ksce function as only plotting function was provided

--- metrics_test.py
+++ metrics_test.py
@@ -2,6 +2,9 @@ import numpy as np
 
 from metrics.ece import expected_calibration_error
 from metrics.fce import fuzzy_calibration_error
+from metrics.tce import tce
+from metrics.tce import tce_ttest
+from metrics.ksce import ksce
 
 n_samples = 10000
 
@@ -24,6 +27,22 @@ print(len(y_pred))
 print(len(y_pred_all_wrong))
 
 n_bins = 10
+
+# y_pred = y_pred_all_wrong
+y_pred = y_pred[:, 1]
+
+print(y_pred)
+
+ksce_val = ksce(scores=y_pred, labels=y_true)
+print("ksce_val: ", ksce_val)
+
+exit()
+
+tce_val = tce(preds=y_pred, labels=y_true, n_min=n_bins, n_max=n_bins, n_bin=n_bins, strategy="uniform")
+print("tce_val: ", tce_val)
+
+tce_ttest_val = tce_ttest(preds=y_pred, labels=y_true, n_min=n_bins, n_max=n_bins, n_bin=n_bins, strategy="uniform")
+print("tce_ttest_val: ", tce_ttest_val)
 
 bins = [10, 50, 100, 1000]
 for bin in bins:
