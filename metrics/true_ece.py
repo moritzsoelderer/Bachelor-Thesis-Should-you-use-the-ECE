478736ee18eb1694a5c6d7fa0201d011f46db7ea
moritzsoelderer <moritzsoelderer@gmail.com>
2024-06-10 12:40:32 +0200

    Corrected mistake in true ece implementation

--- metrics/true_ece.py
+++ metrics/true_ece.py
@@ -9,4 +9,4 @@ def true_ece(scores, true_prob):
     check_scores(true_prob)
     check_shapes(scores, true_prob)
 
-    return sum(abs(np.array(true_prob) - np.array(true_prob))) / len(true_prob)
+    return sum(abs(np.array(scores) - np.array(true_prob))) / len(true_prob)
