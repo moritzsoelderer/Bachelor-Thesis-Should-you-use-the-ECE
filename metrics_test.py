3ac38c01f27c7aa23943c1775fa9147842376a31
moritzsoelderer <moritzsoelderer@gmail.com>
2024-06-14 20:05:45 +0200

    nothing much

--- metrics_test.py
+++ metrics_test.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from metrics import ace
 from metrics.ece import ece
-from metrics.fce import fuzzy_calibration_error
+from metrics.fce import fce
 from metrics.ksce import ksce
 from metrics.tce import tce
 from metrics.tce import tce_ttest
@@ -22,7 +22,7 @@ y_pred_all_wrong = np.array(
     [[label, not label] for label in y_true], dtype=np.float32
 )
 
-y_pred = y_pred_all_wrong
+y_pred = y_pred_all_correct
 
 y_true_ksce = np.array(list(filter(lambda a: a == 1, y_true)))
 y_pred_ksce = np.array([y_pred[i] for i in range(len(y_pred)) if y_true[i] == 1])
@@ -53,7 +53,7 @@ for bin in bins:
     balance_score_val = balance_score(scores=y_pred, y_true=y_true)
     print("balance_score_val: ", balance_score_val)
 
-    _, fce_val = fuzzy_calibration_error(y_true, y_pred, n_bins=bin)
+    fce_val = fce(y_pred, y_true, n_bins=bin)
     print("fce_val ", fce_val)
 
     tce_val = tce(preds=y_pred, labels=y_true, n_min=bin, n_max=bin, n_bin=bin, strategy="uniform")
