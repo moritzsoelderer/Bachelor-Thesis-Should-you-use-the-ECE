0f80ab023120b73ac307f0ea744ff7ef9d9d5eed
moritzsoelderer <moritzsoelderer@gmail.com>
2025-01-04 23:15:31 +0100

    Added imbalanced versions of gummy worm and sad clown datasets

--- src/utilities/data_generation.py
+++ src/utilities/data_generation.py
@@ -338,6 +338,18 @@ def gummy_worm_dataset() -> DataGeneration:
     class_object2 = ClassObject([dist2_1, dist2_2], None)
     return DataGeneration([class_object1, class_object2], n_uninformative_features=0, title="GummyWorm Dataset")
 
+
+def imbalanced_gummy_worm_dataset() -> DataGeneration:
+    dist1_1 = st.multivariate_normal(mean=[10, 10], cov=1, allow_singular=True, seed=42)
+    dist1_2 = st.multivariate_normal(mean=[6, 2], cov=1.7, allow_singular=True, seed=13)
+    dist1_3 = st.multivariate_normal(mean=[7, 10], cov=1, allow_singular=True, seed=165)
+    dist2_2 = st.multivariate_normal(mean=[6, 6], cov=1.7, allow_singular=True, seed=37)
+
+    class_object1 = ClassObject([dist1_1, dist1_2, dist1_3], None)
+    class_object2 = ClassObject([dist2_2], None)
+    return DataGeneration([class_object1, class_object2], n_uninformative_features=0, title="Imbalanced GummyWorm Dataset")
+
+
 def sad_clown_dataset() -> DataGeneration:
     dist1_1 = st.multivariate_normal(mean=[-5, 9, -1.5], cov=4.5, allow_singular=True, seed=33)
     dist1_2 = st.multivariate_normal(mean=[6, -4], cov=8, allow_singular=True, seed=101)
@@ -355,5 +367,26 @@ def sad_clown_dataset() -> DataGeneration:
     )
     class_object2 = ClassObject([dist2_1, dist2_2, dist2_3], None)
     return DataGeneration([class_object1, class_object2], n_uninformative_features=0, title="SadClown Dataset")
+
+
+def imbalanced_sad_clown_dataset() -> DataGeneration:
+    dist1_1 = st.multivariate_normal(mean=[-5, 9, -1.5], cov=4.5, allow_singular=True, seed=33)
+    dist1_2 = st.multivariate_normal(mean=[6, -4], cov=8, allow_singular=True, seed=101)
+    dist1_3 = st.multivariate_normal(mean=[8, -10], cov=5, allow_singular=True, seed=1)
+    dist1_4 = st.multivariate_normal(mean=[4, 4, 4], cov=2.7, allow_singular=True, seed=44)
+
+    dist2_1 = st.multivariate_normal(mean=[7, 10, 3], cov=3, allow_singular=True, seed=57)
+    dist2_2 = st.multivariate_normal(mean=[-8, -4, 5], cov=6, allow_singular=True, seed=92)
+
+    class_object1 = ClassObject([dist1_1, dist1_2, dist1_3, dist1_4],
+    [
+                        MixtureInformation.empty(),
+                        MixtureInformation(features_after=1, features_after_interval=(-20, 10)),
+                        MixtureInformation(features_before=1, features_before_interval=(-15, 10)),
+                        MixtureInformation.empty()
+                    ]
+    )
+    class_object2 = ClassObject([dist2_1, dist2_2], None)
+    return DataGeneration([class_object1, class_object2], n_uninformative_features=0, title="Imbalanced SadClown Dataset")
 
 
