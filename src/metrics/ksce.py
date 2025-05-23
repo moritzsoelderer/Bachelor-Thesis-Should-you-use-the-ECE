f81b410a9856fe089341269c399c03ab520abb62
moritzsoelderer <moritzsoelderer@gmail.com>
2025-05-23 20:40:00 +0200

    Updated powerpoint + comment update

--- src/metrics/ksce.py
+++ src/metrics/ksce.py
@@ -7,7 +7,7 @@ def ksce(scores, labels):
     scores = ensure_numpy(scores)
     labels = ensure_numpy(labels)
 
-    ### Added by myself so that I am not obligated to use a 1d array with positive class labels
+    ### Added by myself so that I am not obligated to use a 1d array with positive class probabilities
     scores = np.array([elem[1] for elem in scores], dtype=np.float32)
     ###
 
